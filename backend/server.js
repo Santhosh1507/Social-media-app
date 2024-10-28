ecace718aba5d44a50d3efcd2db10aa370dda4c1
Santhosh1507 <webdesign1507@gmail.com>
2024-10-28 09:57:34 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const __dirname = path.resolve();
 // Middleware
 app.use(
   cors({
-    origin: "https://social-media-app-one-jade.vercel.app",
+    origin: "http://localhost:5000",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
@@ -49,17 +49,13 @@ app.use("/api/v1/messages", messageRoute);
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 // Serve frontend in production
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "/frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-//   });
-// }
-
-app.get("/", (req, res) => {
-  res.send("API is running...");
-})
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 // Start the server
 server.listen(PORT, () => {
